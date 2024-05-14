7f19a11504eefc1aeaf3dd2b5b640c93cabbefb3
I-iBoy <mprogrammierer@gmail.com>
2024-05-14 20:50:34 +0200

    task 1 (Quadratbaum) (Recursion)

--- Recursion/code/Selbstähnliche_Figurentest.py
+++ Recursion/code/Selbstähnliche_Figurentest.py
@@ -4,7 +4,7 @@ import time
 class App():
     def __init__(self):
         self.turtle = Turtle()
-        self.turtle.speed(12)
+        self.turtle.speed(15)
         self.turtle.left(90)
     
     def draw_tree_1(self, lenght=200):
@@ -20,13 +20,37 @@ class App():
             self.turtle.right(winkel)
             self.turtle.backward(lenght)
     
+    def quadratbaum(self, lenght=200):
+        winkel_1 = 45
+        winkel_2 = 90
+        lenght_2 = lenght / 2
+        
+        if lenght >= 5:
+            self.turtle.right(winkel_2)
+            self.turtle.forward(lenght_2)
+            self.turtle.left(winkel_2)
+            self.turtle.forward(lenght)
+            self.turtle.right(winkel_1)
+            self.quadratbaum(lenght_2)
+            self.turtle.left(winkel_1 + winkel_2)
+            self.turtle.forward(lenght)
+            self.turtle.right(winkel_1)
+            self.quadratbaum(lenght_2)
+            self.turtle.left(winkel_1 + winkel_2)
+            self.turtle.forward(lenght)
+            self.turtle.left(winkel_2)
+            self.turtle.forward(lenght_2)
+            self.turtle.left(winkel_2)
+    
     def run(self):
         
         start_time = time.time()
-        self.draw_tree_1()
+        self.quadratbaum()
         end_time = time.time()
         
         print(f'RUNTIME: {end_time - start_time}')
+        
+        time.sleep(50)
         
         self.turtle.done()
 
