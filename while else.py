854edda324de6c3ed07e93975de7a2495c070251
I-iBoy <mprogrammierer@gmail.com>
2024-05-16 20:42:45 +0200

    while else problem

--- while else.py
+++ while else.py
@@ -1,7 +1,17 @@
 i = 10
 while i > 5:
     print(i)
-    i -= i
+    i -= 1
 else:
     print("i is no longer greater than 5")
+print('end')
+
+print("____________________________\n")
+
+# it does the same? why should sb use this? 
+i = 10
+while i > 5:
+    print(i)
+    i -= 1
+print("i is no longer greater than 5")
 print('end')
