e3f2ae81b2d6f1ab0281392342f643b2e552422e
iBoy <mprogrammierer@gmail.com>
2024-06-09 00:43:35 +0200

    add the README file and a new named version of the application

---
+++ YT_Downloader/README.md
@@ -0,0 +1,57 @@
+# YouTube Downloader with GUI
+
+## The application download a YouTube video from the given link 
+
+This Python application provides a user-friendly graphical interface (GUI) for downloading videos from YouTube. 
+It leverages the pytube library to interact with YouTube's API and the (custom)tkinter libraries for the visually appealing GUI elements.
+
+
+Features:
+
+    Easy Link Entry:                Paste a YouTube video link directly into the provided field.
+    Save Directory Selection:       Choose a specific folder to save your downloaded videos using the convenient "Select Directory" button.
+    Live Progress Bar:              Visually track the download progress as your video is being downloaded.
+    Descriptive Status Updates:     Stay informed about the download process through clear messages displayed in the application.
+    Error Handling:                 The application gracefully handles various YouTube video download failures, providing informative error messages to guide you.
+
+
+Requirements:
+
+    Python 3 (https://www.python.org/downloads/)
+    pytube library (pip install pytube)
+    customtkinter library (pip install customtkinter)
+
+Installation:
+
+    Ensure you have Python 3 installed on your system.
+    Open a terminal or command prompt and navigate to the directory containing this application's code files.
+    Install the required libraries:
+
+    pip install pytube customtkinter pillow
+    or
+    pip3 install pytube customtkinter pillow
+
+
+Usage:
+
+    Run the script using Python:
+        python YT_Downloader.py
+        or 
+        python3 YT_Downloader.py
+
+    Paste a YouTube video link into the designated entry field.
+    Click the "Select Directory" button to choose a save location for your downloaded video (optional, defaults to the current working directory).
+    Click the "Download" button to initiate the download process.
+    The progress bar and status messages will keep you updated as the video is downloaded.
+
+
+Additional Considerations:
+
+    Advanced Features: 
+        You might explore adding options to choose video quality, format, or download multiple videos at once.
+
+    Error Handling Improvement: 
+        Consider providing more specific error messages for different pytube exceptions, such as network connectivity issues or server-side errors.
+    
+    Platform Compatibility: 
+        While customtkinter strives for cross-platform compatibility, it's recommended to test the application on different operating systems.
---
+++ YT_Downloader/YT_Downloader.py
@@ -0,0 +1,155 @@
+import tkinter as tk
+import customtkinter as ttk
+import os
+from tkinter.filedialog import askdirectory
+from tkinter.constants import DISABLED, NORMAL
+import pytube
+from PIL import ImageTk, Image
+import urllib.request
+import io
+import threading
+import pytube.exceptions
+
+class App():
+    def __init__(self):
+        self.root = ttk.CTk()
+        self.root.title("Youtube Downloader")
+        self.root.resizable(False, False)
+        
+        self.root.bind("<Escape>", lambda x: quit())
+        self.root.bind("<Return>", func=lambda x: threading.Thread(daemon=True, target=self.logic).start())
+        self.root.bind('<Button-1>', lambda x: threading.Thread(daemon=True, target=self.display_thumbnail()).start())
+        
+        self.directory_name = os.getcwd()
+        self.surface()
+    
+    def surface(self):
+        # Title
+        self.title = ttk.CTkLabel(self.root, text="Insert a YouTube link")
+        self.title.pack(padx=10, pady=20)
+        
+        # Link Entry
+        self.link = ttk.CTkEntry(self.root, width=550)
+        self.link.pack(padx=10, pady=40)
+        self.link.focus_set()
+        
+        self.link.insert(0,'https://www.youtube.com/watch?v=EEKugIYBEUo')
+        
+        # Save Directory Label
+        self.save_instructor = ttk.CTkLabel(self.root, text=f'Where do you want to save your file? (Default: "{self.directory_name}")')
+        self.save_instructor.pack(padx=10, pady=10)
+        
+        # Save Directory Button
+        self.save = ttk.CTkButton(self.root, text='Select directory to save', command=self.ask_directory)
+        self.save.pack(padx=10, pady=10)
+        
+        # Progress Label
+        self.p_percentage = ttk.CTkLabel(self.root, text="0%")
+        self.p_percentage.pack()
+        
+        # Progress Bar
+        self.progressbar = ttk.CTkProgressBar(self.root, width=550)
+        self.progressbar.set(0)
+        self.progressbar.pack(padx=10, pady=10)
+        
+        # Download Button
+        self.button = ttk.CTkButton(self.root, text="Download", command=lambda: threading.Thread(daemon=True, target=self.logic).start())
+        self.button.pack(padx=10)
+        
+        # Finish Label
+        self.finish_layer = ttk.CTkLabel(self.root, text='')
+        self.finish_layer.pack(padx=10, pady=10)
+        
+        # Thumbnail
+        self.thumbnail = ttk.CTkLabel(self.root, text='Thumbnail', width=320, height=180)
+        self.thumbnail.pack(padx=10, pady=10)
+    
+    def display_thumbnail(self, link=None):
+        if link == None:
+            url = self.link.get()
+            yt = pytube.YouTube(url, on_progress_callback=self.update_progress)
+            link = yt.thumbnail_url
+        
+        # major source: https://www.tutorialspoint.com/displaying-images-from-url-in-tkinter 
+        try:
+            with urllib.request.urlopen(link) as u:
+                raw_data = u.read()
+        except Exception as e:
+            print(f"Error fetching image: {e}")
+            return
+
+        try:
+            image = Image.open(io.BytesIO(raw_data))
+            photo = ttk.CTkImage(image, size=(320,180))
+            self.thumbnail.configure(image=photo, text='')
+        except Exception as e:
+            print(f"Error opening image: {e}")
+            return
+    
+    def ask_directory(self):
+        self.directory_name = askdirectory()
+        self.save_instructor.configure(text=f'Where do you want to save your file? (Current: "{self.directory_name}")')
+        self.finish_layer.configure(text=f"Directory changed to {self.directory_name}")
+        
+        threading.Thread(daemon=True, target=self.display_thumbnail).start()
+    
+    def update_progress(self, stream, size, bytes_remaining): # removed file_handle
+        total_size = stream.filesize
+        bytes_downloaded = total_size - bytes_remaining
+        percentage = bytes_downloaded / total_size
+        self.progressbar.set(value=percentage) 
+        # before it was percentage*100 but the progressbar already operates with percentages
+        self.p_percentage.configure(text=f"{percentage * 100:.2f}%")
+    
+    def logic(self):
+        try:
+            self.progressbar.set(0.0)
+            self.p_percentage.configure(text='0%')
+            self.button.configure(cursor='no', hover=False, state=DISABLED)
+            self.finish_layer.configure(text='Pending...',text_color='white')
+            
+            url = self.link.get()
+            yt = pytube.YouTube(url, on_progress_callback=self.update_progress)
+            threading.Thread(daemon=True, target=self.display_thumbnail, args=(yt.thumbnail_url,)).start()
+            self.stream = yt.streams.get_highest_resolution()
+            self.file_name = os.path.join(self.directory_name, yt.title + ".mp4")
+            self.finish_layer.configure(text='Downloading...')
+            self.stream.download(self.directory_name, yt.title+".mp4", "")
+            
+            # self.button.configure(cursor='arrow')
+            # self.progressbar.set(1.0)
+            # self.p_percentage.configure(text='100%')
+            self.finish_layer.configure(text='Video Downloaded', text_color='green')
+        
+        except pytube.exceptions.VideoUnavailable:
+            self.finish_layer.configure(text='Video unavailable', text_color='red')
+            
+        except pytube.exceptions.LiveStreamError:
+            self.finish_layer.configure(text='Live stream', text_color='red')
+        
+        except pytube.exceptions.AgeRestrictedError:
+            self.finish_layer.configure(text='Age restricted', text_color='red')
+        
+        except pytube.exceptions.MembersOnly:
+            self.finish_layer.configure(text='Members only', text_color='red')
+        
+        except Exception as e:
+            print(e)
+            self.finish_layer.configure(text='Invalid URL', text_color='red')
+        
+        self.button.configure(hover=True, cursor='hand2', state=NORMAL)
+        self.progressbar.set(1.0)
+        self.p_percentage.configure(text='100%')
+        self.root.lift()
+        
+    def run(self):
+        self.root.mainloop()
+
+
+# __________ main code __________
+app=App()
+app.run()
+
+# if __name__ == "__main__": # not practical when converting to .exe
+#     app = App()
+#     app.run()
